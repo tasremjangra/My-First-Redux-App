b2bf606ce2e68d17eed2fe9adfb3c607a6463039
Tarsem Jangra <tarsemk.jangra@gmail.com>
2019-01-02 17:24:18 +0530

    my first React app using Redux

---
+++ src/reducers.js
@@ -0,0 +1,51 @@
+const mainReducer = (state,action) => {
+	switch(action.type){
+		case 'RESET_CONUTER' :
+			return{
+				...state,
+				currentValue:action.newValue
+			}
+
+		case 'DECREMENT' :
+			return{
+				...state,
+				currentValue: state.currentValue+1
+			}
+
+		case 'WIDTH' :
+			return{
+				...state,
+				width:action.newValue,
+				imagePath:action.imagePath
+			}	
+
+		case 'ORIGINAL'	:
+			return{
+				...state,
+				width:action.newValue
+			}
+
+		case 'RED' :
+			return{
+				...state,
+				color:action.newValue
+			}
+
+		case 'GREEN' :
+			return{
+				...state,
+				color: action.newValue
+			}
+
+		case 'BLACK' :
+			return{
+				...state,
+				color:action.color
+			}		
+
+		default:
+		return state;				
+	}
+}
+
+export default mainReducer
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import {createStore} from 'redux'
+import mainReducer from './reducers'
+
+const configureStore = () => {
+	return createStore(mainReducer, {width:400, currentValue:110, color:"black", imagePath:"https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"})
+}
+
+export default configureStore	
